c300f7ee3e992fffe84a6fe96e87c283adfa250f
Gabriel Silva <biel.silva2802@gmail.com>
2024-02-26 23:52:17 -0300

    feat: adicionado seeder para popular o banco usando factory e faker.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,16 +7,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    \App\Models\Address::factory()->count(800)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    \App\Models\User::factory()->count(1000)->create();
+  }
 }
